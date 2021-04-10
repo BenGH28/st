94052212e097aa19473ae674bd18703a46478281
Ben <bigben.hunt@gmail.com>
2021-04-10 14:03:35 -0600

    Add install script and change font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka:size=16:antialias=true:autohint=true:scalable=true:width=2";
+static char *font = "Iosevka Extended:size=16:antialias=true:autohint=true:scalable=true";
 
 /* Spare fonts */
 static char *font2[] = {
