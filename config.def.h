5e4fcfd7fcdcf966648c25473461259ce104b7e3
Ben <bigben.hunt@gmail.com>
2021-04-15 20:24:54 -0600

    change keybindings

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static char *font = "Iosevka Extended:size=16:antialias=true:autohint=true";
 
 /* Spare fonts */
 static char *font2[] = {
-	"FiraCode Nerd Font Mono:pixelsize=16:antialias=true:autohint=true",
+	"JetBrainsMono Nerd Font :pixelsize=16:antialias=true:autohint=true",
 };
 static int borderpx = 2;
 
@@ -211,9 +211,9 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
-	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
+	{ ControlMask,          XK_equal,       zoom,           {.f = +1} },
+	{ ControlMask,          XK_minus,       zoom,           {.f = -1} },
+	{ ControlMask,          XK_0,           zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
